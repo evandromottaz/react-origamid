37bb194c665b3bafc97908800dff22edb5f6a754
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-11-09 15:23:54 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
 import React from 'react';
+import Form from './Form';
 
 const App = () => {
-  // const [nome, setNome] = React.useState('');
-  // const [email, setEmail] = React.useState('');
-
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-  });
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({
-      ...form,
-      [id]: value,
-    });
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(form);
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nome" name="nome">
-        Nome
-      </label>
-      <input
-        type="text"
-        id="nome"
-        name="nome"
-        value={form.nome}
-        onChange={handleChange}
-      />
-      <label htmlFor="email" name="email">
-        Email
-      </label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      {form.nome} e {form.email}
-      <button>Enviar</button>
-    </form>
-  );
+  return <Form />;
 };
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,88 +1,90 @@
 import React from 'react';
 
-const formFields = [
-  {
-    id: 'nome',
-    type: 'text',
-    label: 'Nome',
-  },
-  {
-    id: 'email',
-    type: 'email',
-    label: 'Email',
-  },
-  {
-    id: 'senha',
-    type: 'password',
-    label: 'Senha',
-  },
-  {
-    id: 'cep',
-    type: 'text',
-    label: 'CEP',
-  },
-  {
-    id: 'rua',
-    type: 'text',
-    label: 'Rua',
-  },
-  {
-    id: 'bairro',
-    type: 'text',
-    label: 'Bairro',
-  },
-  {
-    id: 'cidade',
-    type: 'text',
-    label: 'Cidade',
-  },
-  {
-    id: 'estado',
-    type: 'text',
-    label: 'Estado',
-  },
-];
-
 const Form = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-    senha: '',
-    cep: '',
-    rua: '',
-    bairro: '',
-    cidade: '',
-    estado: '',
-  });
+  const [mensagem, setMensagem] = React.useState('');
+
+  const formFields = [
+    {
+      id: 'nome',
+      type: 'text',
+      label: 'Nome',
+    },
+    {
+      id: 'email',
+      type: 'email',
+      label: 'Email',
+    },
+    {
+      id: 'senha',
+      type: 'password',
+      label: 'Senha',
+    },
+    {
+      id: 'cep',
+      type: 'text',
+      label: 'CEP',
+    },
+    {
+      id: 'rua',
+      type: 'text',
+      label: 'Rua',
+    },
+    {
+      id: 'bairro',
+      type: 'text',
+      label: 'Bairro',
+    },
+    {
+      id: 'cidade',
+      type: 'text',
+      label: 'Cidade',
+    },
+    {
+      id: 'estado',
+      type: 'text',
+      label: 'Estado',
+    },
+  ];
+  // loop nos objetos para o reactState
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }, {}),
+  );
   function handleChange({ target }) {
     const { id, value } = target;
-    setForm({
-      ...form,
-      [id]: value,
-    });
+    setForm({ ...form, [id]: value });
   }
 
   function handleSubmit(e) {
-    e.preventDefaul();
+    e.preventDefault();
     fetch('https://ranekapi.origamid.dev/json/api/usuario', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(form),
-    }).then((r) => r.json());
+    }).then((r) =>
+      r.ok
+        ? setMensagem('Enviado com sucesso')
+        : setMensagem('Tente novamente'),
+    );
   }
 
   return (
     <form onSubmit={handleSubmit}>
       {formFields.map(({ id, type, label }) => (
-        <div style={{ background: 'green' }} key={id}>
-          <label style={{ fontSize: '2em' }} htmlFor={label} name={id}>
+        <div key={id}>
+          <label htmlFor={label} name={id}>
             {label}
           </label>
-          <input type={type} id={id} />
+          <input type={type} id={id} value={form[id]} onChange={handleChange} />
         </div>
       ))}
+      <p>{mensagem}</p>
       <button style={{ marginTop: '1rem' }}>Enviar</button>
     </form>
   );
